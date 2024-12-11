d994a6395fd33a832680f077b6399bf8e63f0fa1
Haley Zheng <haleyzheng917@gmail.com>
2024-12-10 20:39:11 -0600

    fix dynamic square sizing and add holding

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 // create initial 16x16 grid'
 function createGrid(size) {
+    const gridSize = size;
+    const squareSize = 800 / gridSize;
     const container = document.querySelector("#container");
-    for (let i = 0; i < size; i++) {
+    for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.classList.add("grid-square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
         container.appendChild(square);
     }
 
 }
 
-const DEFAULT_SIZE = 16*16;
+const DEFAULT_SIZE = 16;
 createGrid(DEFAULT_SIZE);
+let isMouseDown = false;
 
-container.addEventListener("mouseover", function(e) {
-    if (e.target && e.target.classList.contains("grid-square")) {
+container.addEventListener("mousedown", (e) => {
+    if (e.button === 0) {
+        isMouseDown = true;
+        e.preventDefault();
+    }
+    
+});
+
+container.addEventListener("mouseup", () => {
+    isMouseDown = false;
+});
+
+container.addEventListener("mousemove", function(e) {
+    if (isMouseDown && e.target.classList.contains("grid-square")) {
         e.target.style.backgroundColor = "black";
     }
 
 });
 
-
+// user prompt to reset grid
 const resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click", function(){
     let userInput = prompt("Enter the size of the new grid: (1-100)");
@@ -28,6 +45,6 @@ resetButton.addEventListener("click", function(){
     }
     container.innerHTML = "";
     let inputNum = parseInt(userInput);
-    createGrid(inputNum*inputNum);
+    createGrid(inputNum);
 
 });
